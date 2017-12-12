1143530c7ef347c924533cef483db4bc8162c76d
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2017-12-12 10:11:24 -0800

    HelloWorld example in Go

---
+++ hello/hello-world.go
@@ -0,0 +1,11 @@
+// Packages are used to provide code compartmentalisation and reusability
+package main
+
+import (
+	"fmt"	// package to print text to standard output
+)
+
+// Special function for program execution
+func main() {
+	fmt.Println("Hello World")	// Print text to standard output
+}
